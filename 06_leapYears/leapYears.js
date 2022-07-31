34834a80e5ebe168887516d7dc034079e6015aad
Mac <sophanpa@gmail.com>
2022-07-31 11:29:15 +0200

    Edit leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    for(let i = 1904; i <= year;i+=4){
+        if(i == year){
+            return isLeapYear = true;
+        }
+     
+    }
+    if(year%400 == 0){
+        return isLeapYear = true;
+    }
+  return isLeapYear;
 };
 
 // Do not edit below this line
