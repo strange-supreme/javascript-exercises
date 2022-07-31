2e234405312b78a1243fc9a9bc6903d0809b68d8
Mac <sophanpa@gmail.com>
2022-07-31 12:55:58 +0200

    Edit tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  let cel;
+   
+  cel = (num-32) * 5/9
 
+return Math.round(cel * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  let fah;
+  fah = num * 1.8 + 32;
 
+  return Math.round(fah * 10) / 10;;
 };
 
 // Do not edit below this line
