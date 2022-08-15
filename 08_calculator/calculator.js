91eda25b6f7b4d00b994a13c1107d43d13d977ec
Mac <sophanpa@gmail.com>
2022-08-15 22:46:16 +0200

    Finish Task 1

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num,num2) {
+	return num + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num,num2) {
+	return num - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num,num2) {
+	return num + num2;
 };
 
-const multiply = function() {
-
+const multiply = function(num,num2) {
+  return num * num2;
 };
 
-const power = function() {
-	
+const power = function(num,num2) {
+	return Math.pow(num,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fac = 0;
+	for(let i = 1; i<=num;i++){
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
