e28792bff4719cd4afac61a68e0b89d212f20f8d
Mac <sophanpa@gmail.com>
2022-08-18 20:42:06 +0200

    Add if for 1  and for negative numbers, and accept strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,21 @@
 const fibonacci = function(num) {
     let fibo = [0,1];
     let end = 0;
-for(let i = 2; i<=num;i++){
+    num = Number(num);
+    if(num == 1){
+        end = 1;
+    }
+    if(num < 0){
+        return "OOPS";
+    }else {
+        for(let i = 2; i<=num;i++){
    
    fibo[i] = fibo[i-1] + fibo[i-2];
    end = fibo[i];
 }
 return end;
+    }
+
 };
 
 // Do not edit below this line
