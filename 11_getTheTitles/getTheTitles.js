c5042e6504b29ed20353e69ba137daa2b0b1f678
Mac <sophanpa@gmail.com>
2022-08-18 23:25:15 +0200

    Saved titles in array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let title = [];
+    for(let i = 0; i < books.length;i++){
+       title[i] = books[i].title;
+    }
+    return title;
 };
 
 // Do not edit below this line
