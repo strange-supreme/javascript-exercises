72e56dbb042f316153c96f359df9fc0bbd712b6c
Mac <sophanpa@gmail.com>
2022-08-18 20:31:11 +0200

    Edit first logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibo = [0,1];
+    let end = 0;
+for(let i = 2; i<=num;i++){
+   
+   fibo[i] = fibo[i-1] + fibo[i-2];
+   end = fibo[i];
+}
+return end;
 };
 
 // Do not edit below this line
