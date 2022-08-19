4f8e950b027c4cef01d32ced698a56c3dcbf78e3
Mac <sophanpa@gmail.com>
2022-08-19 03:45:51 +0200

    Add date and condition if living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = "";
+    let age = 0;
+    let d = new Date();
+    for(let i = 0; i < people.length;i++){
+        if(people[i].yearOfDeath){
+                    if((people[i].yearOfDeath - people[i].yearOfBirth)>age){
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+            oldest = people[i];
+        }
+        } else {
+            if(d.getFullYear() - people[i].yearOfBirth> age){
+            age = d.getFullYear() - people[i].yearOfBirth;
+            oldest = people[i];
+            }
 
+        }
+
+    }
+    return oldest;
 };
 
 // Do not edit below this line
